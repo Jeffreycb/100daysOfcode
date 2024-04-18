fb0a00bc21e31be4b451020972eb0470c0876c4d
Jeff <166829018+Jeffreycb@users.noreply.github.com>
2024-04-17 20:10:11 -0500

    Create wordGame.py
    #this is from Day7 of course 100 Days of Code: The Complete Python Pro Bootcamp by by Dr. Angela Yu as found on Udemy

---
+++ wordGame.py
@@ -0,0 +1,36 @@
+#this is from Day7 of course 100 Days of Code: The Complete Python Pro Bootcamp by by Dr. Angela Yu as found on Udemy 
+#this file needs some work but it is my first attempt prior to seeing final version from the instructor.
+import random
+bank=["subway", "abruptly", "bandwagon", "microwave", "peekaboo", "pajama", "wave", "jackpot", "money", "buffalo", "buzzard", "kazoo"]
+chosen_word=bank[random.randint(0,len(bank)-1)]
+print(chosen_word)
+list_spaces=[]
+for char in chosen_word:
+    list_spaces+="_"
+print(list_spaces)
+guess=""
+guessed_letters=""
+
+health=100
+x=0
+for _ in range(len(list_spaces)):
+    x+=1
+print (x)
+while health>0 and x>0:
+    guess=input("please choose a letter to guess the word\n").lower()
+    guessed_letters+=guess
+    
+    for position in range(len(chosen_word)):
+        letter=chosen_word[position]
+        if letter == guess:
+            list_spaces[position]=letter
+            x-=1
+            print (x)
+        else:
+            #print("Wrong")
+            health-=1
+    print (list_spaces)
+if health>0:
+    print("You won!")
+else:
+    print("Game over, thanks for playing!")
